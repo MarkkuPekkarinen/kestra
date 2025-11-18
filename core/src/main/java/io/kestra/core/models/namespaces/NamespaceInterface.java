50fb1b0cd9725b7031b22dc4321ae01b2aba2ffd
Loïc Mathieu <loikeseke@gmail.com>
2025-11-18 15:00:33 +0100

    chore(system): remove unused deleted column in logs and metrics

--- core/src/main/java/io/kestra/core/models/namespaces/NamespaceInterface.java
+++ core/src/main/java/io/kestra/core/models/namespaces/NamespaceInterface.java
@@ -2,7 +2,7 @@ package io.kestra.core.models.namespaces;
 
 import io.kestra.core.models.HasUID;
 
-public interface NamespaceInterface extends DeletedInterface, HasUID {
+public interface NamespaceInterface extends HasUID {
     String getId();
 
 
